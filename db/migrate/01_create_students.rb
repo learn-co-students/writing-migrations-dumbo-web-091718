e9cac6f3713d1b9bc4dae09018470e0b4e7057de
github username <github email address>
2018-09-29 13:46:54 -0400

    added grade && birthdate column to tables and migrated

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,18 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+
+  def change
+    create_table :students do |t|
+      t.string :name
+
+      #   rake db:migrate =>
+      # == 1 CreateStudents: migrating ===========
+      # -- create_table(:students) -> 0.0005s
+      # == 1 CreateStudents: migrated (0.0006s) ==
+
+     #then cd db/migrate && touch 02_add_grade_and_birthdate_to_students.rb
+
+    end
+  end
+
 end
